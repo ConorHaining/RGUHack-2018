2bb9e6437f5edd8d97fd5631396af9824b0b6bee
Conor Haining <conor.haining@gmail.com>
2018-04-15 00:01:27 +0100

    Thin out API

--- app/index.js
+++ app/index.js
@@ -38,48 +38,9 @@ app.get('/vessel/MMSI/:mmsi', (req, res) => {
 
 app.get('/vessel/name/:name', (req, res) => {
 
-  db.model.Vessel.findOne({Name: req.params.name}, (err, vessel) => {
-    if (err) throw err;
+  vessel.byName(req.params.name, (ship) => {
 
-    if (vessel === null) {
-      res.sendStatus(404);
-    } else {
-
-      res.render("vessel", {ship: vessel});
-
-    }
-
-  });
-
-});
-
-app.get('/vessel/callsign/:callsign', (req, res) => {
-
-  db.model.Vessel.findOne({CallSign: req.params.callsign}, (err, vessel) => {
-    if (err) throw err;
-
-    if (vessel === null) {
-      res.sendStatus(404);
-    } else {
-
-      res.render("vessel", {ship: vessel});
-
-    }
-
-  });
-
-});
-
-app.get("/api/vessel/callsign/:callsign", (req, res, next) => {
-
-  db.model.Vessel.findOne({CallSign: req.params.callsign}, (err, vessel) => {
-    if (err) throw err;
-
-    if (vessel === null) {
-      res.sendStatus(404);
-    } else {
-      res.send(vessel);
-    }
+    res.render("vessel", {ship: ship});
 
   });
 
@@ -94,53 +55,5 @@ app.get("/api/vessel/name/:name", (req, res) => {
   });
 
 });
-
-app.get("/api/vessel/type/:type", (req, res) => {
-
-  db.model.Vessel.find({Type: req.params.type}, (err, vessel) => {
-    if (err) throw err;
-
-    if (vessel === null) {
-      res.sendStatus(404);
-    } else {
-      res.send(vessel);
-    }
-
-  });
-
-});
-
-app.get("/api/vessel/mmsi/:mmsi", (req, res) => {
-
-  db.model.Vessel.findOne({MMSI: req.params.mmsi}, (err, vessel) => {
-    if (err) throw err;
-
-    if (vessel === null) {
-      res.sendStatus(404);
-    } else {
-
-      db.model.Position.find({MMSI: req.params.mmsi}, "location", {sort: {RecvTime: -1}}, (err, positions) => {
-        if (err) throw err;
-
-        var locations = [];
-
-        for (var i = 0; i < positions.length; i++) {
-          locations[i] = positions[i]['location'];
-        }
-
-        var ship = {
-          vessel: vessel,
-          locations: locations
-        }
-
-        res.json(ship);
-      });
-
-    }
-
-  });
-
-});
-
 
 app.listen(3000, () => console.log("Debugging on port 3000"));
