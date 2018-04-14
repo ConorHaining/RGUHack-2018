088dab149a6d5ec06b07d46921721c933a406bbb
Conor Haining <conor.haining@gmail.com>
2018-04-14 15:11:10 +0100

    Model Acess

---
+++ app/models/models.js
@@ -0,0 +1,7 @@
+// Models
+const mongoose = require("mongoose");
+
+module.exports = {
+	Vessel: mongoose.model("Vessel", require("./Vessel.js")),
+	Position: mongoose.model("Position", require("./Position.js")),
+};
